aea4906d802cf96e6fd4208e7aea2e6306f7f532
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-03 18:12:29 +0900

    Initial Commit

---
+++ GitTestSwiftFile/GitTestSwiftFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestSwiftFileApp.swift
+//  GitTestSwiftFile
+//
+//  Created by David Goggins on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct GitTestSwiftFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

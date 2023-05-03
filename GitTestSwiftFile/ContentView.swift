ecd9d212ba71b58633335c81b4d18c256429bfb5
Hunyong Seong <tjdgnsdyd@gmail.com>
2023-05-03 18:23:57 +0900

    Change text comment and commit this file

--- GitTestSwiftFile/ContentView.swift
+++ GitTestSwiftFile/ContentView.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Body 줄 변화시키기
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("커밋 테스트")
+
         }
         .padding()
     }
-}
+} // 여기서도 또 추가가 되나? 소스컨트롤 누르고 커밋 누르고 커밋확인하고 커밋메세지 적는 곳인데?!
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
